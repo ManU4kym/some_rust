0b5dafbb91bf9dd3a4ec8f23efd88bd60487f778
ManU4kym <emmannjoroge36@gmail.com>
2025-05-13 22:51:55 +0300

    exit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{env::args, process::exit};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+    let name = match args.get(1) {
+        Some(name) => name,
+        None => {
+            println!("Please provide your name");
+            exit(1);
+        }
+    };
+    let profession = match args.get(2) {
+        Some(profession) => profession,
+        None => {
+            println!("Please provide your profession");
+            exit(1);
+        }
+    };
+    println!(
+        "My name is {}, and the profession that i'm in is{}",
+        name, profession
+    );
 }
